c8684c6db0eb31d66fe818af9aeaf0ac5a97cf8f
tractors <huijianhua@163.com>
2020-07-02 21:24:08 +0800

    first commit

---
+++ app/src/main/java/com/will/calculationtest/QuestionFragment.java
@@ -0,0 +1,30 @@
+package com.will.calculationtest;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link QuestionFragment} factory method to
+ * create an instance of this fragment.
+ */
+public class QuestionFragment extends Fragment {
+
+
+    public QuestionFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_question, container, false);
+    }
+}
